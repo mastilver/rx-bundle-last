ccaec9fa2088e5511ca1989b371b29007310878d
Thomas Sileghem <th.sileghem@gmail.com>
2015-08-19 23:33:42 +0100

    feat(bundler): bundle last emitter value

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 'use strict';
 
 
-module.exports = function (observable) {
-    return function (length){
+module.exports = function (source, min, max) {
 
-    };
+    if(max == null){
+        max = min;
+    }
+
+    source = source
+    .scan(function(acc, x){
+        acc.push(x);
+        return acc;
+    }, [])
+    .filter(function(x){
+        return x.length >= min;
+    })
+    .map(function(x){
+        return x.slice(-max);
+    });
+
+    return source
 };
---
+++ test/fixed_bundle_length.js
@@ -0,0 +1,38 @@
+require('should');
+
+var Rx = require('rx');
+
+var rxBundleLast = require('../index');
+
+
+describe('fixed bundle length', function(){
+
+    var result = [];
+
+    before(function(done){
+
+        var source = Rx.Observable.from([1, 2, 3, 4]);
+
+
+        rxBundleLast(source, 2)
+        .subscribe(function(x){
+            result.push(x);
+        }, null, done)
+    });
+
+    it('should emit 3 value', function(){
+        result.length.should.be.equal(3);
+    });
+
+    it('should emit the first value', function(){
+        result[0].should.be.deepEqual([1, 2]);
+    });
+
+    it('should emit the second value', function(){
+        result[1].should.be.deepEqual([2, 3]);
+    });
+
+    it('should emit the third value', function(){
+        result[2].should.be.deepEqual([3, 4]);
+    });
+});
